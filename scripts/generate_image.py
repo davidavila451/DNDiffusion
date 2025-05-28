705b48a98a98b348b117c7305207a8bbf30e36d1
David A. <avila_david_2@yahoo.com>
2025-05-27 21:48:08 -0500

    Prepped the AI to learn
    Prepped the AI to learn

--- scripts/generate_image.py
+++ scripts/generate_image.py
@@ -4,8 +4,4 @@ import torch
 pipe = StableCascadeCombinedPipeline.from_pretrained(
     "davidavila451/dnd-model-SD",
     torch_dtype=torch.float16
-).to("cpu")
-
-#prompt = "a dwarven warrior in golden armor, standing in a misty battlefield"
-#image = pipe(prompt).images[0]
-#image.save("output/dwarf_warrior.png")
+).to("cuda")
